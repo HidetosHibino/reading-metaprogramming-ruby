fb2b82c76162556c4c6babb7fc7e6f231a4cd9be
Hidetoshi Hibino <hibinozzzz@gmail.com>
2023-09-06 09:09:47 +0900

    06_codes_generate_code

--- 06_codes_generate_codes/given/01_simple_model.rb
+++ 06_codes_generate_codes/given/01_simple_model.rb
@@ -14,43 +14,4 @@
 # 3. 履歴がある場合、すべての操作履歴を放棄し、値も初期状態に戻す `restore!` メソッドを作成する
 
 module SimpleModel
-  def self.included(klass)
-    klass.attr_accessor :_histories, :_initial
-    klass.extend(ClassMethods)
-  end
-
-  def initialize(args = {})
-    self._initial = args
-    self._histories = {}
-    args.each do |key, value|
-      instance_variable_set("@#{key}", value)
-    end
-  end
-
-  def restore!
-    self._histories = {}
-    _initial.each do |key, value|
-      instance_variable_set("@#{key}", value)
-    end
-  end
-
-  def changed?
-    !_histories.empty?
-  end
-
-  module ClassMethods
-    def attr_accessor(*syms)
-      syms.each { |sym| attr_reader sym }
-      syms.each do |sym|
-        define_method "#{sym}=" do |value|
-          (_histories[sym] ||= []).push(value)
-          instance_variable_set("@#{sym}", value)
-        end
-
-        define_method "#{sym}_changed?" do
-          !!_histories[sym]
-        end
-      end
-    end
-  end
 end
